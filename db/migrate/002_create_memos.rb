a79e783d32dd859ea3ecd093e3178f3886aeacbf
sa2taka <sa2taka@gmail.com>
2018-05-06 01:10:37 +0900

    変更: 既存マイグレーションファイルのcolumnを一部変更

--- db/migrate/002_create_memos.rb
+++ db/migrate/002_create_memos.rb
@@ -2,9 +2,12 @@ class CreateMemos < ActiveRecord::Migration[4.2]
   def change
     create_table :memos do |t|
       t.belongs_to :user, index: true
-      t.string :memo_status
+      t.text :memo_status
+      t.string :status_id
 
       t.timestamps
     end
+
+    add_index :memos, :status_id
   end
 end
