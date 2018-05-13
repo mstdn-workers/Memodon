50f0bb8a355af4dee2e472da8d38b99fbdd386b8
sa2taka <sa2taka@gmail.com>
2018-05-13 21:56:36 +0900

    修正: memosのidをbigint型に修正

--- db/migrate/002_create_memos.rb
+++ db/migrate/002_create_memos.rb
@@ -1,6 +1,7 @@
 class CreateMemos < ActiveRecord::Migration[4.2] # :nodoc:
   def change
-    create_table :memos do |t|
+    create_table :memos, id: false do |t|
+      t.column :id, 'BIGINT PRIMARY KEY'
       t.belongs_to :user, index: true
       t.text :memo_status
 
