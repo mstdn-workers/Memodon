7f648d4348d337b760607627170920c762454211
sa2taka <sa2taka@gmail.com>
2018-05-13 17:44:27 +0900

    修正: jsの数字の最大値を超えるidをajaxで送っていたものを修正

--- route/api.rb
+++ route/api.rb
@@ -3,6 +3,12 @@ get '/api/memos' do
   user = User.find(session[:user_id])
   memos = user.memo.order('id desc')
   data = { username: user.username, memos: memos }
-  data[:memos] = data[:memos].select { |m| m.memo_status.include? params[:like] } if params[:like]
+  data[:memos] = data[:memos]
+                 .select { |m| m.memo_status.include? params[:like] }
+                 .map do |m|
+                   hash = m.attributes
+                   hash['id'] = m.id.to_s
+                   hash
+                end if params[:like]
   data.to_json
 end
